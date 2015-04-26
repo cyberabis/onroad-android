f30a3705ea4627bd8766848cef831a200ccf40f8
cyberabis <babishek@gmail.com>
2015-04-27 00:14:03 +0530

    fixed bug in manifest

--- app/src/main/java/io/logbase/onroad/SensorTrackerIntentService.java
+++ app/src/main/java/io/logbase/onroad/SensorTrackerIntentService.java
@@ -6,16 +6,23 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.location.Location;
 import android.location.LocationManager;
+import android.os.Bundle;
 import android.provider.Settings;
 import android.support.v4.content.LocalBroadcastManager;
 import android.util.Log;
 
+import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
+import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
 
-public class SensorTrackerIntentService extends IntentService {
+public class SensorTrackerIntentService extends IntentService implements
+        ConnectionCallbacks, OnConnectionFailedListener {
 
     private static final String LOG_TAG = "OnRoad Sensor Tracker";
+    private GoogleApiClient mGoogleApiClient;
+    private static boolean runService = true;
 
     public SensorTrackerIntentService() {
         super("SensorTrackerIntentService");
@@ -24,55 +31,76 @@ public class SensorTrackerIntentService extends IntentService {
     @Override
     protected void onHandleIntent(Intent intent) {
         Log.i(LOG_TAG, "Sensor tracker service started.");
-
-        //Flags
-        boolean runService = true;
-        long frequency = 1000;
-
-        //Check what sensors available
-
-        GoogleApiClient mGoogleApiClient = new GoogleApiClient.Builder(this)
-                .addApi(LocationServices.API)
-                .build();
-        Location mLastLocation = null;
+        runService = true;
+        //Check is GPS is available
         LocationManager lm = (LocationManager) getSystemService(LOCATION_SERVICE);
-        if(!lm.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
-            runService = false;
+        if(lm.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+            mGoogleApiClient = new GoogleApiClient.Builder(this)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .addApi(LocationServices.API)
+                    .build();
+            mGoogleApiClient.connect();
+            long frequency = 1000;
+            Location mLastLocation = null;
+            while(runService) {
+                //Read sensor data and write to file
+                //GPS
+                mLastLocation = LocationServices.FusedLocationApi.getLastLocation(
+                        mGoogleApiClient);
+                Log.i(LOG_TAG, "Location : " + mLastLocation);
+                if (mLastLocation != null) {
+                    //TODO
+                    //Save
+                }
+                //Sleep for a frequency
+                try {
+                    Thread.sleep(frequency);
+                } catch (Exception e) {
+                }
+                //Read flag again
+                SharedPreferences sharedPref = this.getSharedPreferences(getString(R.string.pref_file_key),
+                        Context.MODE_PRIVATE);
+                String toggleMode = sharedPref.getString(getString(R.string.toggle_mode_key), null);
+                if ((toggleMode != null) && (toggleMode.equals(getString(R.string.toggle_trip_start_button)))) {
+                    runService = false;
+                }
+            }
+            mGoogleApiClient.disconnect();
+            Log.i(LOG_TAG, "Disconnecting Google API, stopping sensor tracker service.");
+        } else {
             //Broadcast to activity that the service stopped
             Intent localIntent = new Intent(Constants.BROADCAST_ACTION)
-                            .putExtra(Constants.SERVICE_STATUS, Constants.STOP_STATUS);
+                    .putExtra(Constants.SERVICE_STATUS, Constants.STOP_STATUS);
             // Broadcasts the Intent to receivers in this app.
             LocalBroadcastManager.getInstance(this).sendBroadcast(localIntent);
         }
-
-        while(runService) {
-
-            //Read sensor data and write to file
-            //GPS
-            mLastLocation = LocationServices.FusedLocationApi.getLastLocation(
-                    mGoogleApiClient);
-            Log.i(LOG_TAG, "Location : " + mLastLocation);
-            if (mLastLocation != null) {
-                Log.i(LOG_TAG, "Lat: " + mLastLocation.getLatitude());
-                Log.i(LOG_TAG, "Long: " + mLastLocation.getLongitude());
-            }
-
-            //Sleep for a frequency
-            try {
-                Thread.sleep(frequency);
-            } catch (Exception e) {
-            }
-
-            //Read flag again
-            SharedPreferences sharedPref = this.getSharedPreferences(getString(R.string.pref_file_key),
-                    Context.MODE_PRIVATE);
-            String toggleMode = sharedPref.getString(getString(R.string.toggle_mode_key), null);
-            if ((toggleMode != null) && (toggleMode.equals(getString(R.string.toggle_trip_start_button)))) {
-                runService = false;
-            }
-        }
-
         Log.i(LOG_TAG, "Sensor tracker service stopped.");
+    }
+
+    @Override
+    public void onConnected(Bundle connectionHint) {
+        Log.i(LOG_TAG, "Google API connected");
+        runService = true;
+    }
+
+    @Override
+    public void onConnectionSuspended(int cause) {
+        // The connection has been interrupted.
+        // Disable any UI components that depend on Google APIs
+        // until onConnected() is called.
+        Log.i(LOG_TAG, "Google API connection suspended");
+        runService = false;
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult result) {
+        // This callback is important for handling errors that
+        // may occur while attempting to connect with Google.
+        //
+        // More about this in the next section.
+        Log.i(LOG_TAG, "Google API connection failed");
+        runService = false;
     }
 
 }
