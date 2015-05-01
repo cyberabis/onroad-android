ab1a7150d52a9726f099b8644903e825f45926b9
cyberabis <babishek@gmail.com>
2015-05-01 21:08:44 +0530

    auto file upload changes

--- app/src/main/java/io/logbase/onroad/AutoTrackerIntentService.java
+++ app/src/main/java/io/logbase/onroad/AutoTrackerIntentService.java
@@ -17,6 +17,10 @@ import android.os.Environment;
 import android.support.v4.content.LocalBroadcastManager;
 import android.util.Log;
 
+import com.amazonaws.auth.CognitoCachingCredentialsProvider;
+import com.amazonaws.mobileconnectors.s3.transfermanager.TransferManager;
+import com.amazonaws.mobileconnectors.s3.transfermanager.Upload;
+import com.amazonaws.regions.Regions;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
@@ -58,6 +62,8 @@ public class AutoTrackerIntentService extends IntentService implements
     private Sensor gyroscope = null;
     private boolean slowdownGPS = true;
     private SortedMap<Long, Float> speeds = new TreeMap<Long, Float>();
+    private Upload upload = null;
+    private File uploadFile = null;
 
     public AutoTrackerIntentService() {
         super("AutoTrackerIntentService");
@@ -126,7 +132,42 @@ public class AutoTrackerIntentService extends IntentService implements
     }
 
     private void uploadFiles() {
-        //TODO upload files
+        if (upload == null) {
+            uploadAFile();
+        } else if (upload.isDone()) {
+            // Remove uploaded file and start next upload
+            uploadFile.delete();
+            upload = null;
+            uploadAFile();
+        } else {
+            //DO nothing as upload is in progress
+            Log.i(LOG_TAG, "Upload in progress");
+        }
+    }
+
+    private void uploadAFile() {
+        // Initialize the Amazon Cognito credentials provider
+        File directory = null;
+        File[] files = null;
+        if (isExternalStorageWritable()) {
+            directory = this.getExternalFilesDir(Environment.DIRECTORY_DOCUMENTS);
+        } else {
+            directory = getFilesDir();
+        }
+        if (directory.exists())
+            files = directory.listFiles();
+        if ( (files != null) && (files.length > 0) ) {
+            CognitoCachingCredentialsProvider credentialsProvider = new CognitoCachingCredentialsProvider(
+                    this,
+                    "us-east-1:de6c43db-ed3e-4c40-9c03-f0ba710c669c",
+                    Regions.US_EAST_1
+            );
+            TransferManager transferManager = new TransferManager(credentialsProvider);
+            Log.i(LOG_TAG, "No. of files to upload: " + files.length);
+            uploadFile = files[0];
+            upload = transferManager.upload(Constants.S3_BUCKET_NAME, uploadFile.getName(), uploadFile);
+        } else
+            Log.i(LOG_TAG, "Nothing to upload");
     }
 
     private void startRecording(){
